9319fa6af16dd1fdfbf96c3e07bd08ed08fa2f1e
TaninDean <reiodean.dean@gmail.com>
2023-04-12 20:16:16 +0700

    add get all ticket and validae ticket endpoint

---
+++ ticket_service/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('tickets/', views.get_ticket),
+    path('validate-ticket/', views.validate_ticket)
+]
---
+++ ticket_service/views.py
@@ -0,0 +1,39 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Tickets
+
+
+@api_view(['GET'])
+def get_ticket(request):
+    tickets = Tickets.objects.all()
+    ticket_list = []
+    for i in tickets:
+        ticket = {
+            'id': i.id,
+            'title': i.title,
+            'seatNumber': i.seatNumber,
+            'cinema': i.cinema,
+            'showtime': i.showtime
+        }
+        ticket_list.append(ticket)
+    return Response(data=ticket_list, status=200, content_type='application/json')
+
+
+@api_view(['GET'])
+def validate_ticket(request):
+    ticket_id = request.query_params.get('ticketid')
+    print(ticket_id)
+    try:
+        user = Tickets.objects.get(id=ticket_id)
+        print(user)
+        ticket = {
+            'id': user.id,
+            'title': user.title,
+            'seatNumber': user.seatNumber,
+            'cinema': user.cinema,
+            'showtime': user.showtime
+        }
+    except Tickets.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+    return Response(data=ticket, status=200, content_type='application/json')
+
